79734f5d5960897701835550327f65e94e4f9f83
JackGRWD <j9greenw@uwaterloo.ca>
2022-08-28 12:37:50 -0600

    PWM Timer

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -99,10 +99,13 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
+
+	  HAL_Delay(10);
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
   }
+
   /* USER CODE END 3 */
 }
 
