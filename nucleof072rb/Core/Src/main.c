fb6687d7256676677936d409158925a72a711c05
DJfox <liamsuter0@gmail.com>
2024-10-10 22:01:14 -0400

    New Changes after PR Review
    Thanks, Derek!

--- nucleof072rb/Core/Src/main.c
+++ nucleof072rb/Core/Src/main.c
@@ -23,7 +23,6 @@
 #include "tim.h"
 #include "usart.h"
 #include "gpio.h"
-#include <stdlib.h>
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
@@ -50,15 +49,11 @@
 uint8_t pT_data[3] = {0x01, 0x80, 0x00};
 uint8_t pR_data[3];
 uint16_t size = 3;
-uint32_t timeout = HAL_MAX_DELAY;
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 /* USER CODE BEGIN PFP */
-double ADCToPWM(int ADC_val);
-void decToBinary(long decimal_val, int* fill_array);
-int binaryArrayToDecimal(int binary[], int size);
 
 /* USER CODE END PFP */
 
@@ -97,17 +92,20 @@ int main(void)
   MX_GPIO_Init();
   MX_USART2_UART_Init();
   MX_SPI1_Init();
-  MX_SPI2_Init();
   MX_TIM1_Init();
   /* USER CODE BEGIN 2 */
 
-    __HAL_RCC_GPIOB_CLK_ENABLE();
-  	GPIO_InitTypeDef GPIO_InitStruct = {0};
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_8, GPIO_PIN_SET);
+  __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, (0.05 * __HAL_TIM_GET_AUTORELOAD(&htim1)));
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+
+  	/*GPIO_InitTypeDef GPIO_InitStruct = {0};
   	GPIO_InitStruct.Pin = GPIO_PIN_8;
   	GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   	GPIO_InitStruct.Pull = GPIO_PULLUP;
   	HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
-  	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_8, GPIO_PIN_SET);
+  	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_8, GPIO_PIN_SET);*/
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -116,43 +114,20 @@ int main(void)
   {
 
 	  HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_8);
-	  HAL_SPI_TransmitReceive(&hspi1, pT_data, pR_data, size, timeout);
+	  HAL_SPI_TransmitReceive(&hspi1, pT_data, pR_data, size, HAL_MAX_DELAY);
 	  HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_8);
 
 
-	  /* We fill this array with the first 8 bits received from the ADC that contain USEFUL DATA
-	   * (this is technically the second byte if we're coutning the useless byte received at the start
-	   * NOTE: These bits still need to be parsed through for the useful ones. Only the last 2 bits
-	   * contain useful data so they must be extracted */
-	  int first_8_bits[8];
 
 
-	  /* We fill this array with the last 8 bits received from the ADC */
-	  int last_8_bits[8];
-
-	  /* Converts the decimal notations of the bytes into binary */
-	  decToBinary((int)pR_data[1], first_8_bits);
-	  decToBinary((int)pR_data[2], last_8_bits);
-
-
-	  int ADC_binary[10];
-	  /* We extract all the useful bits and put them into one array
-	   * We are using LSB first so we fill the ADC_binary array from last index to first
+	  /* We filter out the first 6 useless bits from the second byte using a bitmask
+	   * We then shift the 2 useful bits to the leftmost side of a 10bit sequence before combining it
+	   * with the third byte received from the ADC using bit-OR
 	   */
-	  for(int i = 0; i < 2; i++) {
-		  ADC_binary[9-i] = first_8_bits[6+i];
-	  }
-
-	  for(int i = 0; i < 8; i++) {
-		  ADC_binary[7-i] = last_8_bits[i];
-	  }
-
-
-	  /* We convert the full binary info into a decimal number that we can convert into our desired PWM signal */
-	  int ADC_val = binaryArrayToDecimal(ADC_binary, 10);
+	  int ADC_val = ((pR_data[1]&0b0000000011) << 8)|pT_data[2];
 
 	  /* Set compare register */
-	  __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, ADCToPWM(ADC_val));
+	  __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, (0.05 + (ADC_val * 0.05 / 1023)) * __HAL_TIM_GET_AUTORELOAD(&htim1));
 	  HAL_Delay(10);
     /* USER CODE END WHILE */
 
@@ -204,37 +179,6 @@ void SystemClock_Config(void)
 
 /* USER CODE BEGIN 4 */
 
-double ADCToPWM(int ADC_val) {
-	return ((ADC_val / 1023) * 3000) + 3000;
-}
-
-void decToBinary(long decimal_val, int fill_array[]) {
-	int curVal = decimal_val;
-	int i = 0;
-
-	while (curVal > 0) {
-		if (decimal_val % 2 == 0) {
-			fill_array[i] = 0;
-		}
-		else {
-			fill_array[i] = 1;
-		}
-		curVal = curVal / 2;
-		i++;
-	}
-}
-
-int binaryArrayToDecimal(int binary[], int size) {
-    int decimalValue = 0;
-
-    // Iterate through the binary array
-    for (int i = 0; i < size; i++) {
-        // Calculate the contribution of each bit
-        decimalValue = decimalValue * 2 + binary[i];
-    }
-
-    return decimalValue;
-}
 
 
 /* USER CODE END 4 */
--- nucleof072rb/Core/Src/spi.c
+++ nucleof072rb/Core/Src/spi.c
@@ -25,7 +25,6 @@
 /* USER CODE END 0 */
 
 SPI_HandleTypeDef hspi1;
-SPI_HandleTypeDef hspi2;
 
 /* SPI1 init function */
 void MX_SPI1_Init(void)
@@ -46,7 +45,7 @@ void MX_SPI1_Init(void)
   hspi1.Init.CLKPhase = SPI_PHASE_1EDGE;
   hspi1.Init.NSS = SPI_NSS_SOFT;
   hspi1.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_16;
-  hspi1.Init.FirstBit = SPI_FIRSTBIT_LSB;
+  hspi1.Init.FirstBit = SPI_FIRSTBIT_MSB;
   hspi1.Init.TIMode = SPI_TIMODE_DISABLE;
   hspi1.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
   hspi1.Init.CRCPolynomial = 7;
@@ -59,40 +58,6 @@ void MX_SPI1_Init(void)
   /* USER CODE BEGIN SPI1_Init 2 */
 
   /* USER CODE END SPI1_Init 2 */
-
-}
-/* SPI2 init function */
-void MX_SPI2_Init(void)
-{
-
-  /* USER CODE BEGIN SPI2_Init 0 */
-
-  /* USER CODE END SPI2_Init 0 */
-
-  /* USER CODE BEGIN SPI2_Init 1 */
-
-  /* USER CODE END SPI2_Init 1 */
-  hspi2.Instance = SPI2;
-  hspi2.Init.Mode = SPI_MODE_MASTER;
-  hspi2.Init.Direction = SPI_DIRECTION_2LINES;
-  hspi2.Init.DataSize = SPI_DATASIZE_8BIT;
-  hspi2.Init.CLKPolarity = SPI_POLARITY_LOW;
-  hspi2.Init.CLKPhase = SPI_PHASE_1EDGE;
-  hspi2.Init.NSS = SPI_NSS_SOFT;
-  hspi2.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_16;
-  hspi2.Init.FirstBit = SPI_FIRSTBIT_LSB;
-  hspi2.Init.TIMode = SPI_TIMODE_DISABLE;
-  hspi2.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
-  hspi2.Init.CRCPolynomial = 7;
-  hspi2.Init.CRCLength = SPI_CRC_LENGTH_DATASIZE;
-  hspi2.Init.NSSPMode = SPI_NSS_PULSE_ENABLE;
-  if (HAL_SPI_Init(&hspi2) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN SPI2_Init 2 */
-
-  /* USER CODE END SPI2_Init 2 */
 
 }
 
@@ -133,39 +98,6 @@ void HAL_SPI_MspInit(SPI_HandleTypeDef* spiHandle)
 
   /* USER CODE END SPI1_MspInit 1 */
   }
-  else if(spiHandle->Instance==SPI2)
-  {
-  /* USER CODE BEGIN SPI2_MspInit 0 */
-
-  /* USER CODE END SPI2_MspInit 0 */
-    /* SPI2 clock enable */
-    __HAL_RCC_SPI2_CLK_ENABLE();
-
-    __HAL_RCC_GPIOC_CLK_ENABLE();
-    __HAL_RCC_GPIOB_CLK_ENABLE();
-    /**SPI2 GPIO Configuration
-    PC2     ------> SPI2_MISO
-    PC3     ------> SPI2_MOSI
-    PB10     ------> SPI2_SCK
-    */
-    GPIO_InitStruct.Pin = GPIO_PIN_2|GPIO_PIN_3;
-    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-    GPIO_InitStruct.Pull = GPIO_NOPULL;
-    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
-    GPIO_InitStruct.Alternate = GPIO_AF1_SPI2;
-    HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
-
-    GPIO_InitStruct.Pin = GPIO_PIN_10;
-    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-    GPIO_InitStruct.Pull = GPIO_NOPULL;
-    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
-    GPIO_InitStruct.Alternate = GPIO_AF5_SPI2;
-    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
-
-  /* USER CODE BEGIN SPI2_MspInit 1 */
-
-  /* USER CODE END SPI2_MspInit 1 */
-  }
 }
 
 void HAL_SPI_MspDeInit(SPI_HandleTypeDef* spiHandle)
@@ -191,27 +123,6 @@ void HAL_SPI_MspDeInit(SPI_HandleTypeDef* spiHandle)
   /* USER CODE BEGIN SPI1_MspDeInit 1 */
 
   /* USER CODE END SPI1_MspDeInit 1 */
-  }
-  else if(spiHandle->Instance==SPI2)
-  {
-  /* USER CODE BEGIN SPI2_MspDeInit 0 */
-
-  /* USER CODE END SPI2_MspDeInit 0 */
-    /* Peripheral clock disable */
-    __HAL_RCC_SPI2_CLK_DISABLE();
-
-    /**SPI2 GPIO Configuration
-    PC2     ------> SPI2_MISO
-    PC3     ------> SPI2_MOSI
-    PB10     ------> SPI2_SCK
-    */
-    HAL_GPIO_DeInit(GPIOC, GPIO_PIN_2|GPIO_PIN_3);
-
-    HAL_GPIO_DeInit(GPIOB, GPIO_PIN_10);
-
-  /* USER CODE BEGIN SPI2_MspDeInit 1 */
-
-  /* USER CODE END SPI2_MspDeInit 1 */
   }
 }
 
