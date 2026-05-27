335e3106b6a070dbeca1e617e072ae06bb85b585
JackyLiuu <Jacky.lxinhao@gmail.com>
2026-05-27 19:53:25 -0400

    Code Generation

--- nucleof072rb/Core/Src/main.c
+++ nucleof072rb/Core/Src/main.c
@@ -49,12 +49,8 @@
 uint8_t spi_tx_data[3] = {0x01, 0x80, 0x00};
 uint8_t spi_rx_data[3] = {};
 uint16_t pwmvalue = 0;
+uint16_t adc_value = 0;
 
-HAL_GPIO_WritePin(SPI1_CS_GPIO_Port, SPI1_CS_Pin, GPIO_PIN_RESET);
-HAL_SPI_TransmitReceive(&hspi1, spi_tx_data, spi_rx_data, 3, HAL_MAX_DELAY);
-HAL_GPIO_WritePin(SPI1_CS_GPIO_Port, SPI1_CS_Pin, GPIO_PIN_SET);
-
-uint16_t adc_value = ((spi_rx_data[1] & 0x03) << 8) | spi_rx_data[2];
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -100,7 +96,7 @@ int main(void)
   MX_SPI1_Init();
   MX_TIM1_Init();
   /* USER CODE BEGIN 2 */
-
+ HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_1);
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -111,7 +107,8 @@ int main(void)
 HAL_GPIO_WritePin(SPI1_CS_GPIO_Port, SPI1_CS_Pin, GPIO_PIN_RESET);
 HAL_SPI_TransmitReceive(&hspi1, spi_tx_data, spi_rx_data, 3, HAL_MAX_DELAY);
 HAL_GPIO_WritePin(SPI1_CS_GPIO_Port, SPI1_CS_Pin, GPIO_PIN_SET);
-uint16_t adc_value = ((spi_rx_data[1] & 0x03) << 8) | spi_rx_data[2];
+
+adc_value = ((spi_rx_data[1] & 0x03) << 8) | spi_rx_data[2];
 pwmvalue = 3200 + ((uint32_t)adc_value * (6400 - 3200)) / 1023;
 __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, pwmvalue);
 
