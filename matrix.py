8369a65bc2be360f4586c2ac002645bb5ae233f2
DanielKolev <dkolev654@gmail.com>
2026-06-05 17:17:55 -0400

    Bootcamp: Daniel Kolev

---
+++ matrix.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+# Edit this matrix with your coefficients and constants:
+# Example for: 
+# 2x - 2y + z = -3
+#  x + 3y - 2z = 1
+# 3x -  y -  z = 2
+augmented_matrix = np.array([
+    [2.0, -2.0,  1.0, -3.0],
+    [1.0,  3.0, -2.0,  1.0],
+    [3.0, -1.0, -1.0,  2.0]
+], dtype=float)
